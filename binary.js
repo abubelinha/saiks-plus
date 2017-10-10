3f711af58754820be26141d4e4adc768c1e8a8c5
Kobe Van de Sande <kobe.vandesande@vrt.be>
2017-10-10 21:47:57 +0200

    Uppercase binary datasets

--- binary.js
+++ binary.js
@@ -5,19 +5,19 @@ var exclusive_mode = true;
 var remove_mode = false;
 
 var chars = [["unused"],
-    ["space 1", "yes", "no"],
-    ["space 2", "yes", "no"],
-    ["space 3", "yes", "no"],
-    ["space 4", "yes", "no"]
+    ["space 1", "Yes", "No"],
+    ["space 2", "Yes", "No"],
+    ["space 3", "Yes", "No"],
+    ["space 4", "Yes", "No"]
 ];
 
 
 var items = [[""],
-    ["yyyn", "1", "1", "1", "2"],
-    ["yyny", "1", "1", "2", "1"],
-    ["yynn", "1", "1", "2", "2"],
-    ["ynyn", "1", "2", "1", "2"],
-    ["ynnn", "1", "2", "2", "2"],
-    ["nynn", "2", "1", "2", "2"],
-    ["nnyn", "2", "2", "1", "2"]
+    ["YYYN", "1", "1", "1", "2"],
+    ["YYNY", "1", "1", "2", "1"],
+    ["YYNN", "1", "1", "2", "2"],
+    ["YNYN", "1", "2", "1", "2"],
+    ["YNNN", "1", "2", "2", "2"],
+    ["NYNN", "2", "1", "2", "2"],
+    ["NNYN", "2", "2", "1", "2"]
 ];
